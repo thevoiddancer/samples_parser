7d574275b74350a0e174f9da6d04d6f609099cfe
thevoiddancer <tomislav.nazifovic@gmail.com>
2023-08-07 00:20:25 +0200

    Contained strings

--- src/samples_parser/contained_string.py
+++ src/samples_parser/contained_string.py
@@ -1,5 +1,14 @@
 from lark import Lark
 from rich import print as rprint
+from samples_parser.grammars import grm_imports, grm_contained_str, grm_terminals
 
 # Grammar for grabbing quoted (single and double) and bracketed (braced, paranthesised) strings
+# Works on string contained within "'([{. Does not handle those characters inside the string.
+# E.g. fails on "this is (parenthesis)"
 
+grammar_start = r"""
+start: contained_string"""
+
+grammar = grammar_start + grm_contained_str + grm_terminals + grm_imports
+
+contained_string_parser = Lark(grammar=grammar)
---
+++ src/samples_parser/grammars.py
@@ -0,0 +1,18 @@
+grm_contained_str = r"""
+contained_string: "\"" STRING "\""
+                  | "'" STRING "'"
+                  | "(" STRING ")"
+                  | "{" STRING "}"
+                  | "[" STRING "]"
+
+"""
+
+grm_terminals = r"""
+STRING: (LETTER | NUMBER | " " | "," | "." | "-" | "!" | "?")+
+
+"""
+
+grm_imports = r"""
+%import common.LETTER
+%import common.NUMBER
+"""
