5cf1b9f979b5b3efd70378c5c70664a184ec33cc
thevoiddancer <tomislav.nazifovic@gmail.com>
2023-08-08 19:53:09 +0200

    Basic transformer done, tweaking needed

--- src/samples_parser/basic_structure.py
+++ src/samples_parser/basic_structure.py
@@ -1,19 +1,23 @@
-from lark import Lark
+import re
+from functools import reduce
+from lark import Lark, Transformer
 from samples_parser.grammars import grm_contained_str
 
 grammar_basic = r"""
 start: [_NL] source_block
 
-source_block: source_info sanple_info song_info
+source_block: source_info sample_info song_info
 
-source_info: source_rank " " source_name source_points source_usage _NL
-sanple_info: " "+ contained_string " " contained_string _NL
-song_info: band "; " album "; " song
+source_info: source_rank " " source_name " " source_points source_usage _NL
+sample_info: " "+ sample_text " " sample_note _NL
+song_info: " "+ "- " band "; " album "; " song
 
 source_rank: INT "."
 source_name: STRING
 source_points: "[" INT " points] "
 source_usage: "(" source_usage_groups source_usage_songs source_usage_samples")"
+sample_text: contained_string
+sample_note: contained_string
 
 source_usage_groups: INT " groups, "
 source_usage_songs: INT " songs, "
@@ -33,3 +37,24 @@ STRING: (LETTER | NUMBER | " " | "," | "." | "-" | "!" | "?" | ":" | _NL)+
 grammar = grammar_basic + grm_contained_str
 
 basic_structure_parser = Lark(grammar=grammar)
+
+class BasicStructureTransformer(Transformer):
+    def compress_space(self, string):
+        return re.sub(r'\s+', ' ', string)
+
+    def __default__(self, data, children, meta):
+        if data in ['source_rank', 'source_points', 'source_usage_groups', 'source_usage_songs', 'source_usage_samples']:
+            return {str(data): int(children[0])}
+        elif data in ['source_name', 'band', 'album', 'song', 'sample_text', 'sample_note']:
+            return {str(data): str(children[0])}
+        elif data in ['source_usage', 'source_info', 'sample_info', 'song_info', 'source_block']:
+            return reduce(lambda x, y: dict(x, **y), children)
+        elif data == 'contained_string':
+            return self.compress_space(children[0])
+        elif data == 'start':
+            return children
+        return super().__default__(data, children, meta)
+    
+    pass
+
+basic_structure_transformer = BasicStructureTransformer()
--- tests/test_basic_structure.py
+++ tests/test_basic_structure.py
@@ -1,11 +1,49 @@
-from samples_parser.basic_structure import basic_structure_parser
+import sys
+import pytest
+from rich import print as rprint
 
-text = """
+from samples_parser.basic_structure import (
+    basic_structure_parser,
+    basic_structure_transformer,
+)
+
+basic_text = """
 1. Blade Runner [738 points] (87 groups, 116 songs, 221 samples)
   "Move on, move on." (Note: Police robot addressing crowd gathering after
    a shootout)
     - Age of Chance; This is Crush Collision; One Thousand Years of Trouble
 """
 
-def test_basic_structure():
-    basic_structure_parser.parse(text)
+basic_tranformed = [
+  {
+    'source_rank': 1,
+    'source_name': 'Blade Runner',
+    'source_points': 738,
+    'source_usage_groups': 87,
+    'source_usage_songs': 116,
+    'source_usage_samples': 221,
+    'sample_text': 'Move on, move on.',
+    'sample_note': 'Note: Police robot addressing crowd gathering after a shootout',
+    'band': 'Age of Chance',
+    'album': 'This is Crush Collision',
+    'song': 'One Thousand Years of Trouble'
+  }
+]
+
+@pytest.mark.parametrize(
+  "text, expected",
+  [
+    pytest.param(basic_text, basic_tranformed, id='Basic sample type')
+  ]
+)
+def test_basic_structure(text, expected):
+  parsed = basic_structure_parser.parse(text)
+  transformed = basic_structure_transformer.transform(parsed)
+  with open('output.txt', 'w') as f:
+    original_stdout = sys.stdout
+    sys.stdout = f
+    rprint(transformed)
+    sys.stdout = original_stdout
+
+  assert transformed == expected
+
